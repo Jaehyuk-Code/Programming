70dc5ca57a3da8b6cf3b1cfd96cc50d51cf356fc
jaehyukcoder <78639218+jaehyukcoder@users.noreply.github.com>
2021-02-17 00:27:55 +0900

    Variable
    2021-02-17

---
+++ Variable/Variable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("이름 : ");
+            string name = Console.ReadLine();
+            Console.Write("나이 : ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("키 : ");
+            float height = float.Parse(Console.ReadLine());
+
+            Console.Write(age);
+            Console.WriteLine(name);
+            Console.WriteLine(height);
+        }
+    }
+}
