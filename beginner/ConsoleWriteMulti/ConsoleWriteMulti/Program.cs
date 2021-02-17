6a7f673298cde8874ccc437a1b6e767054083d4f
jaehyukcoder <78639218+jaehyukcoder@users.noreply.github.com>
2021-02-17 22:14:56 +0900

    Console.WriteLine
    2021-02-17

---
+++ beginner/ConsoleWriteMulti/ConsoleWriteMulti/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWriteMulti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10이하의 소수 : {0}, {1}, {2}, {3} ", 2, 3, 5, 7);
+
+            string primes;
+            primes = string.Format("10이하의 소수 : {0}, {1}, {2}, {3}", 2, 3, 5, 7);
+            Console.WriteLine(primes);
+        }
+    }
+}
